fbd3f4acf3e65c43703989388c67fa44ccd1007e
Elysia-aimen <15138276954@163.com>
2025-01-21 21:38:31 +0800

    Site updated: 2025-01-21 21:38:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/18/2025-01-15-即刻短文/","2025/01/12/blog_update_problems/","2024/12/13/hello-world/","2024/12/19/依稀记得的老师说的话/","2025/01/15/update_/","2025/01/18/日记系列/"];function toRandomPost(){
+var posts=["2025/01/18/2025-01-15-即刻短文/","2025/01/12/blog_update_problems/","2024/12/13/hello-world/","2024/12/19/依稀记得的老师说的话/","2025/01/18/日记系列/","2025/01/15/update_/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
