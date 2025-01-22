8815f6cbbe1a64f09e67d645486aa9d4d65791ac
Elysia-aimen <15138276954@163.com>
2025-01-22 19:56:45 +0800

    Site updated: 2025-01-22 19:56:44

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/12/blog_update_problems/","2024/12/13/hello-world/","2025/01/15/update_/","2025/01/21/notes/","2025/01/18/2025-01-15-即刻短文/","2025/01/18/日记系列/","2024/12/19/依稀记得的老师说的话/"];function toRandomPost(){
+var posts=["2025/01/18/2025-01-15-即刻短文/","2024/12/13/hello-world/","2025/01/21/notes/","2025/01/18/日记系列/","2024/12/19/依稀记得的老师说的话/","2025/01/15/update_/","2025/01/12/blog_update_problems/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
